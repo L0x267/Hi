1119c5860908b99279feefb9e92f03240ee783e4
L0x267 <153518329+L0x267@users.noreply.github.com>
2023-12-12 20:45:10 +0800

    Update hello.py
    remove unnecessary logic

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-a, b, c, d = d, b, c, a = 1, 0, 1, 0
+d, b, c, a = 1, 0, 1, 0 
 e = [[not not a or c, not c, not not not c, not not d or a, 0], [not a or (c or d), not d, a and d, d, 1], [not a, not not c, not not not b, not not not not d, 2], [not not a or c, not c, not not not c, not not d or a, 3], [not a or (c or d), not d, a and d, d, 4], [not a, not a and (b or d), not not c, not not not not d, 5]]
 for index, jndex, kndex, i, j in e:
     print('H' if int(index) and j <= 4 else ('I' if int(index) and j > 4 else ' '), 'H' if int(jndex) and j <= 4 else ('I' if int(index) and j > 4 else ' '), 'H' if int(kndex) and j <= 4 else ('I' if int(index) and j > 4 else ' '), 'H' if int(i) and j <= 4 else ('I' if int(index) and j > 4 else ' '), end=('\n' if (j + 1) % 5 != int(0 and (1 or 0)) else '\n\n'))
